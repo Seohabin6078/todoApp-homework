389e0befa8e17e58f5f88951e27bc5a03d5bc4ca
Seohabin6078 <habin6078@gmail.com>
2023-04-12 16:58:09 +0900

    add SampleController

---
+++ src/main/java/com/codestates/todoApphomework/SampleController.java
@@ -0,0 +1,13 @@
+package com.codestates.todoApphomework;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "To-do Application !";
+    }
+}
---
+++ src/test/java/com/codestates/todoApphomework/TodoAppHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.todoApphomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoAppHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
